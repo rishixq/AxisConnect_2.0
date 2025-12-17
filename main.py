e6f6c20557834ff5eecb583678a82f8b16554d4f
Rishi Kishore <149399458+rishixq@users.noreply.github.com>
2025-12-17 21:47:06 +0530

    Update CORS settings to allow all origins

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="AxisConnect API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://axisconnect.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
